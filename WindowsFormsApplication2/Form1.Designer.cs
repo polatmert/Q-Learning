423c616a476b4dee40898a22b7a422b00af54218
polatmert <husnumertpolatt@gmail.com>
2017-04-03 00:36:09 +0300

    Q-Learning Algorithm
    proje tamamlandı

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Hesapla = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(597, 31);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(233, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Labirent Çiz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(29, 165);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(801, 388);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Hesapla
+            // 
+            this.Hesapla.Location = new System.Drawing.Point(331, 31);
+            this.Hesapla.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Hesapla.Name = "Hesapla";
+            this.Hesapla.Size = new System.Drawing.Size(259, 70);
+            this.Hesapla.TabIndex = 2;
+            this.Hesapla.Text = "Hesapla";
+            this.Hesapla.UseVisualStyleBackColor = true;
+            this.Hesapla.Click += new System.EventHandler(this.Hesapla_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(951, 165);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(217, 356);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.Visible = false;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(159, 20);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(108, 22);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Başlangıç";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 64);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Çıkış";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(159, 55);
+            this.numericUpDown2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(108, 22);
+            this.numericUpDown2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 101);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Iterasyon Sayısı";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Increment = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Location = new System.Drawing.Point(159, 98);
+            this.numericUpDown3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            1215752192,
+            23,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(108, 22);
+            this.numericUpDown3.TabIndex = 9;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Location = new System.Drawing.Point(951, 529);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(139, 30);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "İterasyonlar";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1209, 567);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Hesapla);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Hesapla;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,421 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+//HÜSEYİN EMRE ÖZHAN -140201062
+//HÜSNÜ MERT POLAT   -140201001
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        StreamReader sr; 
+        int OdaSayisi ;
+        int OdaSayisiKoku;
+
+
+        int[,] Rmatrisi;
+        double[,] Qmatrisi;
+        int[] ENKISAYOL;
+
+        int START = 0;
+        int FINISH = 0;
+        int ITERASYON = 100;
+
+
+        public void DENEME(String denenecek)
+        {
+            listBox1.Items.Add(denenecek);
+        }
+        
+        
+        //Gonderilen oda numarasının gidebileği odalarla arasındaki Max olanı bulum döndürmeli
+     public int MaxQ(int MaxQBulunacakOda,int yasakOda )
+        {
+          
+
+            double MaxQDegeri = 0;
+            double mevcutDeger=0;
+            int MaxQIndis = 0;
+            int sonraki=0;
+
+            for (sonraki = 0; sonraki < OdaSayisi; sonraki++)
+            {
+                mevcutDeger=Rmatrisi[MaxQBulunacakOda, sonraki];
+               
+               if (mevcutDeger != (-1) && sonraki != MaxQBulunacakOda && sonraki!=yasakOda)
+                {
+
+                    if(mevcutDeger >= MaxQDegeri )
+                    {
+                        MaxQDegeri = mevcutDeger;
+                        MaxQIndis = sonraki;
+                    }
+                    
+                }
+            }
+
+            return MaxQIndis;
+        }
+
+        public void ENKISAYOLUHESAPLA()
+        { 
+            int []GeciciPath=new int [OdaSayisi];
+            int pathSayac = 0;
+            //STARTI  ekrana yazdır
+            ///STARTDAN başla Q matrisini START Satırının elemanlarını tara en yüksek elemanın Sutun numarasını al YAZDIR o numara FINISH mı ona bak fınıshise bitir
+            /// buldugun  numarayı Q matrisinin satırına atla o satırdaki en büyük elemanı bul sutun numarasını al YAZDIR o numara FINISH mı ona bak
+            DENEME("Baslangıc-"+START.ToString());
+            double EnBuyuk = 0;
+            int EnBuyukIndis = 0;
+            int sayac=0;
+            int satir = START;
+            
+
+            for (int dongu = 0; dongu < OdaSayisi;dongu++ )
+            {
+                for (sayac = 0; sayac < OdaSayisi; sayac++)
+                {
+                    if ( Qmatrisi[satir, sayac] >= EnBuyuk)
+                    {
+                        EnBuyuk = Qmatrisi[satir, sayac];
+                        EnBuyukIndis = sayac;
+                    }
+
+                }
+
+                GeciciPath[pathSayac] = EnBuyukIndis;
+                pathSayac++;
+                DENEME(EnBuyukIndis.ToString());//listboxa yazdırma
+
+                
+              
+                satir = EnBuyukIndis;
+
+                if (satir == FINISH) 
+                    break;
+
+            }
+            //Enkısayol pathi gecici diziden kopyalanarak  ENKISAYOL dizisi elde edilir
+            ENKISAYOL = new int[pathSayac+1];
+            ENKISAYOL[0] = START;
+            for(int jj=0;jj<pathSayac;jj++)
+            {
+                ENKISAYOL[jj+1] = GeciciPath[jj];
+            }
+
+
+
+
+        }
+
+        
+        public void ENKISAYOLUCIZ(Graphics g)
+        {
+           Pen pn =new Pen(Color.Red);
+           pn.Width = 5;
+            int X1, X2, Y1, Y2;
+            int sayac=0;
+
+            while(sayac<ENKISAYOL.GetUpperBound(0))
+            { 
+                X1 = (ENKISAYOL[sayac] % OdaSayisiKoku) * 40 + 20;
+                Y1 = (ENKISAYOL[sayac] / OdaSayisiKoku) * 40 + 20;
+
+
+
+                X2 = (ENKISAYOL[sayac+1] % OdaSayisiKoku) * 40 + 20;
+                Y2 = (ENKISAYOL[sayac+1] / OdaSayisiKoku) * 40 + 20;
+
+              
+                g.DrawLine(pn, X1, Y1, X2, Y2);
+                sayac++;
+                
+            }
+        }
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void DikBeyazCizgi(Graphics g,int CizYY,int CizXX)
+        {g.DrawLine(new Pen(Color.Transparent,3), 40*CizXX ,40*CizYY ,40*CizXX ,40*CizYY+40);}
+        
+        public void DikSiyahCizgi(Graphics g, int CizYY, int CizXX)
+        {g.DrawLine(new Pen(Color.Black,3), 40 * CizXX, 40 * CizYY, 40 * CizXX, 40 * CizYY + 40); }
+        
+        public void YataySiyahCizgi(Graphics g, int CizYY, int CizXX)
+        { g.DrawLine(new Pen(Color.Black,3), 40 * CizXX, 40 * CizYY, 40 * CizXX + 40, 40 * CizYY);}
+        
+        public void YatayMaviCizgi(Graphics g,int CizYY,int CizXX)
+        {g.DrawLine(new Pen(Color.Transparent,3), 40 * CizXX, 40 * CizYY, 40 * CizXX  +40 ,  40 * CizYY);}
+
+      
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            //picture box çizgi cizme deneme
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics g=Graphics.FromImage(bmp);
+            
+               
+                g.DrawRectangle(new Pen(Color.Black,3), 0, 0, 40 * OdaSayisiKoku, 40 * OdaSayisiKoku);
+                //Grid Oluşturma
+
+                //Dikey boşlukları oluşturma
+                for (int oda = 0; oda < OdaSayisi-1; oda++)
+                {
+                    if (Rmatrisi[oda, oda + 1] == 0 || Rmatrisi[oda, oda + 1] == 100)
+                    {
+                        DikBeyazCizgi(g, oda/OdaSayisiKoku, (oda+1)%OdaSayisiKoku);
+                    }
+                    else
+                       DikSiyahCizgi(g, oda / OdaSayisiKoku, (oda + 1) % OdaSayisiKoku);
+                }
+
+
+                //Yatay Boşluklar oluşturma
+                for (int oda = 0; oda < OdaSayisi - OdaSayisiKoku; oda++)
+                {
+                    if (Rmatrisi[oda, oda + OdaSayisiKoku] == 0 || Rmatrisi[oda, oda + OdaSayisiKoku] == 100)
+                    {
+                        YatayMaviCizgi(g, (oda / OdaSayisiKoku)+1, (oda) % OdaSayisiKoku);
+                    }
+                    else
+                        YataySiyahCizgi(g, (oda / OdaSayisiKoku) + 1, (oda) % OdaSayisiKoku);
+                }
+            
+            
+            //EN KISA YOLU ÇİZİYORUZ
+            ENKISAYOLUCIZ(g);
+
+
+            pictureBox1.Image = bmp;
+        }
+
+
+
+
+
+        private void Hesapla_Click(object sender, EventArgs e)
+        {
+            
+            ///Numericlerden  Başlangıç ve Bitiş konumlarını ve Iterasyon Sayısı Alma 
+            START = Convert.ToInt32(numericUpDown1.Value);
+            FINISH = Convert.ToInt32(numericUpDown2.Value);
+            ITERASYON = Convert.ToInt32(numericUpDown3.Value);
+
+            
+            if (START == 0 &&( FINISH == 0 || ITERASYON == 0))
+            {
+                MessageBox.Show("Lütfen Değerleri Belirleyiniz");
+            }
+            else
+            {
+
+
+
+                sr = new StreamReader("input.txt");
+                OdaSayisi = File.ReadLines("input.txt").Count();
+                OdaSayisiKoku = Convert.ToInt32(Math.Sqrt(Convert.ToDouble(OdaSayisi)));
+
+                String dummy = OdaSayisi.ToString();
+                DENEME("satir sayi " + dummy);
+
+                Rmatrisi = new int[OdaSayisi, OdaSayisi];
+                Qmatrisi = new double[OdaSayisi, OdaSayisi];
+
+
+                ///R matrisinin elemanlarını başlangıç için -1 yapıyoruz
+                ///geçiş olanları daha sonra 0'a çeviricez
+                int ii = 0, jj = 0;
+                for (ii = 0; ii < OdaSayisi; ii++)
+                {
+                    for (jj = 0; jj < OdaSayisi; jj++)
+                    { Rmatrisi[ii, jj] = -1; }
+                }
+                ///Qmatrisini 0'lıyoruz
+                for (ii = 0; ii < OdaSayisi; ii++)
+                {
+                    for (jj = 0; jj < OdaSayisi; jj++)
+                    { Qmatrisi[ii, jj] = 0; }
+                }
+
+
+
+                for (int ii2 = 0; ii2 < OdaSayisi; ii2++)
+                {
+                    String OkunanDeger = sr.ReadLine();
+                    //DENEME("Okunan Satir="+OkunanDeger);
+                    String[] Parcalanmis = OkunanDeger.Split(',');//okunan satırı virgullere göre parçalıyor
+
+                    for (int jj2 = 0; jj2 < Parcalanmis.GetUpperBound(0) + 1; jj2++)
+                    {
+                        int Degistirilecek = Convert.ToInt32(Parcalanmis[jj2]);
+
+                        if (Degistirilecek == FINISH)
+                            Rmatrisi[ii2, Degistirilecek] = 100;
+                        else
+                            Rmatrisi[ii2, Degistirilecek] = 0;
+                    }
+                }
+                Rmatrisi[FINISH, FINISH] = 100;//hedefin kendine dönüşü 100kazançlıdır
+                //R matrisi oluşturuldu
+
+                int sayac = 0;
+                int durum = START;
+                int aksiyon = 0;
+
+                Random rnd = new Random();
+
+
+                for (sayac = 0; sayac < ITERASYON; sayac++)
+                {
+                    //aksiyonu belirlemek için
+                    //  for (int kk = 0; kk < OdaSayisi; kk++)
+                    //    if (Rmatrisi[START, kk] != -1)
+                    //      aksiyon = kk;
+                    //aksiyonu belirlemek için
+                    while (true)
+                    {
+                        aksiyon = rnd.Next(0, OdaSayisi);
+                        if (Rmatrisi[START, aksiyon] != -1)
+                            break;
+                    }
+
+                    durum = START;
+
+                    int arasayac = 0;
+                    while (durum != FINISH && arasayac<OdaSayisi)
+                    {
+                        int sonrakiAksiyon = MaxQ(aksiyon, durum);
+                        Qmatrisi[durum, aksiyon] = Rmatrisi[durum, aksiyon] + 0.8 * Qmatrisi[aksiyon, sonrakiAksiyon];
+
+                        DENEME(durum.ToString() + " " + aksiyon.ToString());
+
+                        durum = aksiyon;
+                        aksiyon = sonrakiAksiyon;
+
+                        arasayac++;
+                    }
+                    DENEME("BİR ITERASYON BİTTİ");
+                }
+
+
+                DENEME("enkısa yol___");
+                ENKISAYOLUHESAPLA();
+
+                
+
+
+
+                DENEME("bitti");
+            }
+            button1.Enabled = true;
+
+            OutputDosyasıOlusturma();
+        }
+
+        public void OutputDosyasıOlusturma()
+        {
+
+            //Eğer dosya daha önceden yapılmış ile kontrol eder ve siler 
+            if (File.Exists("Routput.txt"))
+            { File.Delete("Routput.txt"); }
+
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter("Routput.txt", true))
+            {
+                
+                file.Write("R Matrisi:\n");
+                int satır=0,sutun=0;
+                for (satır = 0; satır < OdaSayisi ;satır++ )
+                {
+                    for (sutun = 0; sutun < OdaSayisi; sutun++)
+                    {
+                        file.Write(Rmatrisi[satır,sutun]);
+                        file.Write("    ");
+                    }
+                    file.WriteLine("\n");
+
+                }
+
+      
+            }
+
+             if (File.Exists("Qoutput.txt"))
+            { File.Delete("Qoutput.txt"); }
+
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter("Qoutput.txt", true))
+            {
+                file.Write("Q Matrisi:\n");
+                int satır = 0, sutun = 0;
+                for (satır = 0; satır < OdaSayisi; satır++)
+                {
+                    for (sutun = 0; sutun < OdaSayisi; sutun++)
+                    {
+                        file.Write(Qmatrisi[satır, sutun]);
+                        file.Write("    ");
+                    }
+                    file.WriteLine("\n");
+                }
+
+            }
+
+
+             if (File.Exists("Path.txt"))
+            { File.Delete("Path.txt"); }
+
+             using (System.IO.StreamWriter file = new System.IO.StreamWriter("Path.txt", true))
+             {
+                 file.Write("En Kısa Yol \n");
+                 int sayac=0;
+                for(sayac=0;sayac < ENKISAYOL.GetUpperBound(0)+1;sayac++ )
+                {
+                    file.Write(ENKISAYOL[sayac]);
+                    file.Write("    ");
+                }
+
+             
+             }
+
+
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+                listBox1.Visible = true;
+            else
+                listBox1.Visible = false;
+
+        }
+
+       
+
+    }
+    
+}
